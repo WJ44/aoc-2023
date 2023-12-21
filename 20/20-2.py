6f88499513ac2a116868043338af7ad604d4b763
Wesley Joosten <github@wjoosten.nl>
2023-12-21 12:42:28 +0100

    day 20

---
+++ 20/20-2.py
@@ -0,0 +1,53 @@
+import re
+from collections import deque
+
+modules = {}
+states = {}
+with open("./20/input.txt", "r") as file:
+    for line in file:
+        type, name, destinations = re.match(r"([%&]?)([a-z]+) -> (.*)", line).groups()
+        destinations = destinations.split(", ")
+        modules[name] = (type, destinations)
+        if type == "%":
+            states[name] = -1
+        elif type == "&":
+            states[name] = {}
+
+for name, (_, destinations) in modules.items():
+    for destination in destinations:
+        if destination in modules and modules[destination][0] == "&":
+            states[destination][name] = -1
+
+stack = deque()
+i = 0
+found = False
+from time import sleep
+while True:
+    i += 1
+    if found:
+        break
+    stack.append(("broadcaster", -1, "button"))
+    while stack:
+        name, pulse, source = stack.popleft()
+        if name == "rx" and pulse == -1:
+            found = True
+            break
+        if name not in modules:
+            continue
+        type, destinations = modules[name]
+        send = True
+        if type == "%":
+            if pulse == -1:
+                states[name] *= pulse
+                pulse = states[name]
+            else:
+                send = False
+        elif type == "&":
+            states[name][source] = pulse
+            pulse = -1 if sum(states[name].values()) == len(states[name].values()) else 1
+        if send:
+            stack.extend((module, pulse, name) for module in destinations)
+    print(states["hr"])
+    sleep(0.01)
+
+print(i)
