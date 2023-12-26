0ab6beda6f176d5f06fd92f367f20d0090df4a65
Wesley Joosten <wesleyjoosten@hotmail.nl>
2023-12-26 12:18:21 +0100

    day 24-1

---
+++ 24/24-1.py
@@ -0,0 +1,22 @@
+from itertools import combinations
+
+equations = []
+
+with open("./24/input.txt", "r") as file:
+    for line in file:
+        position, velocity = line.split(" @ ")
+        px, py, pz = position.split(", ")
+        vx, vy, vz = velocity.split(", ")
+        a = int(vy)/int(vx)
+        b = -a*int(px) + int(py)
+        equations.append((a, b, int(px), int(vx)))
+
+count = 0
+for (a, b, px1, vx1), (c, d, px2, vx2) in combinations(equations, 2):
+    if a != c:
+        x = (d - b)/(a - c)
+        y = a*x + b
+        if (x-px1)/vx1 > 0 and (x-px2)/vx2 > 0 and x >= 200000000000000 and x <= 400000000000000 and y >= 200000000000000 and y <= 400000000000000:
+            count += 1
+
+print(count)
